21c55187fbc0cc397c4f1c004eb1451aeb5b8b12
ws410678 <ws410678@CL-WG-R037-D07>
2026-03-31 13:14:56 +0100

    First Push

--- OOP_Project/Program.cs
+++ OOP_Project/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace OOP_Project
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // TODO: Replace with your application entry logic.
+        }
+    }
+}
