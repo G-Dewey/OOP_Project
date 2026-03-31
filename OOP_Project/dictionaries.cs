593386d64d741fadc492c47ce7e9d2ad17e58715
ws410678 <ws410678@CL-WG-R037-D07>
2026-03-31 15:14:30 +0100

    added csv 1st commint

---
+++ OOP_Project/dictionaries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    public static class Dictionaries
+    {
+        // Used to track the subDivision name to an int 
+        public static Dictionary<string, int> subdivisionDict = new Dictionary<string, int>();
+
+        // Used to track the headers and the index in job array
+        public static Dictionary<string, int> headerDict = new Dictionary<string, int>
+        {
+            {"jobid", 0},
+            {"operation", 1},
+            {"subdivision", 2},
+            {"processingtime", 3}
+        };
+    }
+}
---
+++ OOP_Project/fileReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+using static OOP_Project.Dictionaries;
+
+namespace OOP_Project
+{
+    interface IFileReader
+    {
+        public ErrorOr<int[]> ReadFile(string filePath);
+    }
+
+
+    class CSVReader : IFileReader
+    {
+        public ErrorOr<int[]> ReadFile(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    return Error.NotFound("File not found.");
+                }
+
+                // Clear the subdivision dictionary before reading a new file to avoid conflicts with previous data
+                Dictionaries.subdivisionDict.Clear();
+
+                StreamReader reader = new StreamReader(filePath);
+
+                var lines = File.ReadAllLines("data.csv");
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Error.Unexpected(ex.Message);
+            }
+        }
+
+        private ErrorOr<int[]> headerOrder(string[] headers)
+        {
+            int[] indexArray = new int[headers.Length];
+            int index = 0;
+
+            foreach (string header in headers)
+            {
+                string trimmed = header.Trim().ToLower();
+
+                if (Dictionaries.headerDict.ContainsKey(trimmed))
+                {
+                    return Error.NotFound($"Header '{trimmed}' is not recognized.");
+                }
+
+                indexArray[index] = Dictionaries.headerDict[trimmed];
+            }
+
+            return indexArray;
+        }
+    }
+}
+
+
